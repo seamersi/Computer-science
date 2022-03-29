c72b8f54adeaa85789ec74cf20190076a291bc31
seamersi <101748100+seamersi@users.noreply.github.com>
2022-03-29 14:50:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
 function userName(){
     var r = prompt(`Hello welcome to the 20 game what's your name`)
+    return r;
 }
-var uN = userName()
+
+
 
 var userPoints = 0
 
 var computerPoints = 0
 
 function userNumber(){
-    var rN = Math.floor(Math.random() * 20) + 1
-    return;
+    var rN = Math.floor(Math.random() * 10) + 1
+    return rN;
 }
-var un = userNumber()
-function finalUsernumber(io){
-    var addnumber = 0 + io
-    return;
+
+
+var usern = 0
+var compn = 0
+
+function n() {
+    do {
+        usern = usern + userNumber()
+        var n = prompt(`alright ${uN} you are at ${usern} would you like to draw again y/n`)
+    } while ((usern < 20) && (n == `y`))
+
+    return usern;
 }
-var fUn = finalUsernumber()
-function oneEleven(){
-    if(un == 1){
-        var un = 11
-        if(fUn + un > 20){
-            un = 1 
-        }
+
+
+var uN = userName()
+
+
+function p(){
+    do{
+        compn = compn + userNumber()
+        alert(`the computer is at ${compn}`)
+    }while(compn < 16)
+
+    return compn;
+}
+
+var p = p()
+var n = n()
+function compare(){
+
+    if((p > n) && (p > 20) && (n > 20)){
+        alert(`ok ${uN} you finished with ${n} and the computer ended with ${p} that means the computer wins`)
+        computerPoints = computerPoints + 1
+    }
+    else{
+        alert(`ok ${uN} you finished with ${n} and the computer ended with ${p} that means the you win`)
+        userPoints = userPoints + 1
     }
 }
-function alltogehter(){
-    var onemore = finalUsernumber(userNumber(oneEleven()))
-    return;
+
+function playAgain(){
+    do{
+        compare()
+        var pa = prompt(`ok ${uN} you have won ${userPoints} games and the computer has won ${computerPoints} games do you still want to play y/n`)
+    }
+    while(pa == `y`)
+    var p = p()
+    var n = n()
     
 }
-function playagain(){
-    do{
-        var wantToplay = prompt(`You are at ${alltogehter()} would you like to draw cards y/n`)
-    }while(wantToplay == "y")
-    finalUsernumber(userNumber(oneEleven()))
-}
-playagain()
+playAgain()
