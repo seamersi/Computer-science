af0e50bc88a1cc91d54d1769ecffbd7850755700
seamersi <101748100+seamersi@users.noreply.github.com>
2022-03-31 14:32:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function userName(){
     var r = prompt(`Hello welcome to the 20 game what's your name`)
     return r;
 }
-
+var uN = userName()
 
 
 var userPoints = 0
@@ -15,53 +15,50 @@ function userNumber(){
 }
 
 
-var usern = 0
-var compn = 0
 
-function n() {
-    do {
-        usern = usern + userNumber()
-        var n = prompt(`alright ${uN} you are at ${usern} would you like to draw again y/n`)
-    } while ((usern < 20) && (n == `y`))
+do{
 
-    return usern;
-}
+    function n() {
+        var usern = 0
+        do {
+            usern = usern + userNumber()
+            var n = prompt(`alright ${uN} you are at ${usern} would you like to draw again y/n`)
+        } while ((usern < 20) && (n == `y`))
 
-
-var uN = userName()
-
-
-function p(){
-    do{
-        compn = compn + userNumber()
-        alert(`the computer is at ${compn}`)
-    }while(compn < 16)
-
-    return compn;
-}
-
-var p = p()
-var n = n()
-function compare(){
-
-    if((p > n) && (p > 20) && (n > 20)){
-        alert(`ok ${uN} you finished with ${n} and the computer ended with ${p} that means the computer wins`)
-        computerPoints = computerPoints + 1
+        return usern;
     }
-    else{
-        alert(`ok ${uN} you finished with ${n} and the computer ended with ${p} that means the you win`)
-        userPoints = userPoints + 1
-    }
-}
 
-function playAgain(){
-    do{
-        compare()
-        var pa = prompt(`ok ${uN} you have won ${userPoints} games and the computer has won ${computerPoints} games do you still want to play y/n`)
+
+
+
+
+    function p(){
+        var compn = 0
+        do{
+            compn = compn + userNumber()
+            alert(`the computer is at ${compn}`)
+        }while(compn < 16)
+
+        return compn;
     }
-    while(pa == `y`)
-    var p = p()
-    var n = n()
-    
-}
-playAgain()
+    var r = n()
+    var t = p()
+
+    function compare(){
+
+        if((t > r) && (t <= 20) || (r > 20)){
+            alert(`ok ${uN} you finished with ${r} and the computer ended with ${t} that means the computer wins`)
+            computerPoints = computerPoints + 1
+        }
+        else if((r > t) &&(r <=20) || (t > 20)){
+            alert(`ok ${uN} you finished with ${r} and the computer ended with ${t} that means the you win`)
+            userPoints = userPoints + 1
+        }
+        else if((t == r) || (t > 20) && (r > 20)){
+            alert(`ok ${uN} you finished with ${r} and the computer ended with ${t} that means the you tied`)
+        }
+    }
+    compare()
+    var playAgain = prompt(`ok ${uN} you have won ${userPoints} games and the computer has won ${computerPoints} games do you still want to play y/n`)
+}   
+while( playAgain == `y`)
